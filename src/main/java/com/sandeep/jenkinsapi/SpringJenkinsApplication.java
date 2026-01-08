261169cfbba308ffbe8bba3ae57b5ca86d2adce9
javasandeepsingh <java.sandeepsingh@gmail.com>
2026-01-09 01:51:04 +0530

    first time code commit

---
+++ src/main/java/com/sandeep/jenkinsapi/SpringJenkinsApplication.java
@@ -0,0 +1,13 @@
+package com.sandeep.jenkinsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJenkinsApplication.class, args);
+	}
+
+}
